8d0bc20278b2488f98bfe0a031feab2d84f05668
Muhammet Akvardar <34101771+sreilus@users.noreply.github.com>
2020-04-01 23:53:18 +0300

    Generic Repository Pattern Added

---
+++ ShopApp.DataAccess/Concrete/EfCore/ShopContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+	public class ShopContext:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Data Source=.;Integrated Security=True;Database=ShopDb; Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+		}
+
+		public DbSet<Product> Products{ get; set; }
+		public DbSet<Category> Categories{ get; set; }
+	
+	}
+}
